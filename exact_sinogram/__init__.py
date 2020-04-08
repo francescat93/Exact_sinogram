d0e79a28a9e100ab272f3ba13eb5058e045bdb49
pog87 <pog87@email.it>
2020-04-08 20:59:40 +0200

    package made to export

--- exact_sinogram/__init__.py
+++ exact_sinogram/__init__.py
@@ -1,2 +1,3 @@
 from .radon_exact import *
 from .radon_approx import *
+__version__ = '0.1.0'
---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name='exact_sinogram',
+      version='0.1.0',
+      description='A Python package for the computation of the exact sinogram of different cathegories of phantoms for CT reconstruciton',
+      url='http://github.com/francescat93/Exact_sinogram',
+      author='Monica Dessole, Marta Gatto, Davide Poggiali, Francesca Tedeschi',
+      author_email='davide.poggiali@unipd.it',
+      license='MIT',
+      packages=['exact_sinogram'],
+      install_requires=[
+          'matplotlib', 'numpy>=1.13', 'scipy',
+          'math', 'scikit-image'
+      ],
+      zip_safe=False)
